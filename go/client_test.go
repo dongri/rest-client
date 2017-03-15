8392d11ed50f16b150643d6e8976ab5217bd1d52
Dongri Jin <dongrify@gmail.com>
2017-03-15 09:14:28 +0900

    Add yourserver

--- go/client_test.go
+++ go/client_test.go
@@ -2,14 +2,14 @@ package client
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 )
 
 const timeout = 90 //second
+const endpoint = "http://localhost:8080"
 
 func TestGet(t *testing.T) {
-	client := NewClient("https://yourserver.herokuapp.com", timeout)
+	client := NewClient(endpoint, timeout)
 	params := map[string][]string{
 		"page": {"1"},
 	}
@@ -19,9 +19,60 @@ func TestGet(t *testing.T) {
 	}
 	defer res.Body.Close()
 
-	resBody := map[string]interface{}{}
-	if err := json.NewDecoder(res.Body).Decode(resBody); err != nil {
+	var resBody interface{}
+	if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
 		t.Errorf("got error %v", err)
 	}
-	fmt.Println(resBody["age"])
+	t.Log(resBody)
+}
+
+func TestPost(t *testing.T) {
+	client := NewClient(endpoint, timeout)
+	params := map[string][]string{
+		"name":  {"dognri"},
+		"email": {"dognri@domain.com"},
+	}
+	res, err := client.Post("/users", params)
+	if err != nil {
+		t.Errorf("got error %v", err)
+	}
+	defer res.Body.Close()
+	var resBody interface{}
+	if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
+		t.Errorf("got error %v", err)
+	}
+	t.Log(resBody)
+}
+
+func TestPut(t *testing.T) {
+	client := NewClient(endpoint, timeout)
+	params := map[string][]string{
+		"name":  {"dognri"},
+		"email": {"dognri@domain.com"},
+	}
+	res, err := client.Put("/users", params)
+	if err != nil {
+		t.Errorf("got error %v", err)
+	}
+	defer res.Body.Close()
+	var resBody interface{}
+	if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
+		t.Errorf("got error %v", err)
+	}
+	t.Log(resBody)
+}
+
+func TestDelete(t *testing.T) {
+	client := NewClient(endpoint, timeout)
+	params := map[string][]string{}
+	res, err := client.Delete("/users/1", params)
+	if err != nil {
+		t.Errorf("got error %v", err)
+	}
+	defer res.Body.Close()
+	var resBody interface{}
+	if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
+		t.Errorf("got error %v", err)
+	}
+	t.Log(resBody)
 }
