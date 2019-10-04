a1d6c919ec810c7c3e2fdee474587bbad23c32b3
Dongri Jin <dongrify@gmail.com>
2019-10-04 23:03:32 +0900

    Fix

--- server/main.go
+++ server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -80,33 +79,4 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
-}
-
-func parseRequest(r *http.Request) (interface{}, error) {
-	var p interface{}
-	if r.Method != http.MethodPost {
-		return nil, errors.New("Unsupported method error")
-	}
-	contentType := strings.Split(r.Header.Get("Content-Type"), ";")[0]
-	if contentType == "application/json" {
-		length, err := strconv.Atoi(r.Header.Get("Content-Length"))
-		if err != nil {
-			return nil, err
-		}
-		body := make([]byte, length)
-		length, err = r.Body.Read(body)
-		if err != nil && err != io.EOF {
-			return nil, err
-		}
-		if err = json.Unmarshal(body[:length], &p); err != nil {
-			return nil, err
-		}
-	} else if contentType == "application/x-www-form-urlencoded" {
-		if err := r.ParseForm(); err != nil {
-			return nil, err
-		}
-	} else {
-		return nil, errors.New("Unsupported Content-Type")
-	}
-	return &p, nil
 }
